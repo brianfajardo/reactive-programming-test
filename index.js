e2a9f68edd8d9790a23af2bc867417c34186445b
Brian Fajardo <bmf5@hotmail.com>
2020-04-08 17:42:43 -0400

    Add emit method on Observable class

--- index.js
+++ index.js
@@ -6,10 +6,18 @@ class Observable {
   subscribe(fn) {
     this.callbacks.push(fn)
   }
+
+  emit(x) {
+    this.callbacks.map((fn) => fn(x))
+  }
 }
 
 const observable = new Observable()
 
 observable.subscribe((x) => console.log(x))
 
-console.log(observable)
+observable.emit(1)
+observable.emit(2)
+observable.emit(3)
+observable.emit(4)
+observable.emit(5)
